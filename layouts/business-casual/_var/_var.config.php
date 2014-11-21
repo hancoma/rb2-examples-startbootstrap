0759ddb5d2390ded1143ab66c6e82d9e0226ef5f
gitaeks <break@kimsq.com>
2014-11-21 16:40:28 +0900

    update business-casual

---
+++ layouts/business-casual/_var/_var.config.php
@@ -0,0 +1,41 @@
+<?php
+if(!defined('__KIMS__')) exit;
+ 
+$d['layout']['show'] = true;   /** 관리패널에 레이아웃 관리탭을 보여주기 **/
+ 
+
+/***********************************************************************************
+설정배열
+***********************************************************************************/
+ 
+$d['layout']['dom'] = array(
+ 
+ 
+	/** 도움말 **/
+	'help' => array(
+		'Guide',
+		'<h4>Start Bootstrap Business Casual</h4>
+		This theme features a landing page, about page, blog page, and a contact page along with various custom styles and components.
+		<hr>
+		<h5>Source code</h5>
+		<ul>
+			<li><a href="https://github.com/IronSummitMedia/startbootstrap-business-casual" target="_blank">Design template</a></li>
+		 	<li><a href="https://github.com/kimsQ/rb2-examples-startbootstrap/tree/master/layouts/business-casual" target="_blank">Rb2 layout</a></li>
+		 </ul>
+
+		<h5>Demo</h5>
+		<ul>
+			<li><a href="http://ironsummitmedia.github.io/startbootstrap-business-casual/" target="_blank">Design template</a></li>
+		 	<li><a href="http://demo.kimsq.com/rb2/business-casual/" target="_blank">Rb2 site</a></li>
+		 </ul>
+
+		<h5>Docs</h5>
+		<ul>
+		 	<li><a href="http://docs.kimsq.com/rb2/docs/c/examples/start-bootstrap/business-casual" target="_blank">Setting Guide</a></li>
+		 </ul>',
+		array(),
+	),
+);
+ 
+//***********************************************************************************
+?>
